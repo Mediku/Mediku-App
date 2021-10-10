ab67df9c5b1da4a6aebcffd80737118ff4acf628
Handson Panggabean <hansenpanggabean8@gmail.com>
2021-10-11 00:06:13 +0700

    -

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const cors = require('cors')
-// const router = require('./routes')
+const routers = require('./routers')
 const app = express()
 // const PORT = process.env.PORT || 9000
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// app.use(router)
+app.use(routers)
 
 // app.listen(PORT, () => {
 //     console.log('GASKEUNN!!!');
