0b679388551e76012a9a41384a84ed367f12aa1a
Alfian Rudi Yogatama <alvmarjinal@gmail.com>
2021-10-16 22:11:58 +0700

    Foldering Client - Server(User)

--- routers/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const routerUser = require('./routerUser')
-const ErrorHandler = require('../middlewares/ErrorHandler.js')
-
-router.use('/', routerUser)
-
-router.use(ErrorHandler)
-
-module.exports = router
