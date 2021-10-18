f3514a9d199ebd812ab1f42bf1131d30e877882d
JoenathanSent <jonathansentosa80@gmail.com>
2021-10-18 22:51:46 +0700

    gabungin server clinic dengan user

--- server/app/middlewares/ErrorHandler.js
+++ server/app/middlewares/ErrorHandler.js
@@ -11,12 +11,18 @@ module.exports = function (err, req, res, next) {
     });
     code = 400;
     message = errors;
-  } else if (err.name === "Invalid Token") {
+  } else if (err.name === "Unauthorized") {
+    code = 401;
+    message = "Email/Password is wrong";
+  } else if (err.name === "JsonWebTokenError") {
     code = 401;
     message = "Invalid Token";
   } else if (err.name === "Please Login First") {
     code = 401;
     message = "Please Login First";
+  } else if (err.name === "Data Not Found") {
+    code = 404;
+    message = "Data Not Found";
   } else if (err.name === "Not Found") {
     code = 404;
     message = "Clinic not found";
--- server/app/routers/routerUser.js
+++ server/app/routers/routerUser.js
@@ -3,7 +3,6 @@ const ControllerUser = require("../Controllers/ControllerUser");
 const authentication = require("../middlewares/authentication");
 
 router.post("/login", ControllerUser.login);
-console.log("ada error");
 router.post("/register", ControllerUser.register);
 router.post("/auth/google", ControllerUser.googleAuthLogin);
 router.use(authentication);
