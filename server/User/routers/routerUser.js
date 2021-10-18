6ad38c15818931894b7d9f1308bfe7f0adda4776
JoenathanSent <jonathansentosa80@gmail.com>
2021-10-18 21:46:25 +0700

    develop server clinic

--- server/User/routers/routerUser.js
+++ server/User/routers/routerUser.js
@@ -1,13 +1,14 @@
-const router = require('express').Router()
-const ControllerUser = require('../Controllers/ControllerUser')
-const authentication = require('../middlewares/authentication')
+const router = require("express").Router();
+const ControllerUser = require("../Controllers/ControllerUser");
+const authentication = require("../middlewares/authentication");
 
-router.post('/login', ControllerUser.login)
-router.post('/register', ControllerUser.register)
-router.post('/auth/google', ControllerUser.googleAuthLogin)
-router.use(authentication)
-router.get('/user', ControllerUser.findUserLogin)
-router.delete('/users/:id', ControllerUser.deleteUser)
-router.put('/edit/profile', ControllerUser.editUserProfile)
+router.post("/login", ControllerUser.login);
+console.log("ada error");
+router.post("/register", ControllerUser.register);
+router.post("/auth/google", ControllerUser.googleAuthLogin);
+router.use(authentication);
+router.get("/user", ControllerUser.findUserLogin);
+router.delete("/users/:id", ControllerUser.deleteUser);
+router.put("/edit/profile", ControllerUser.editUserProfile);
 
-module.exports = router
+module.exports = router;
