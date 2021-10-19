c66b31a0b538614fe646662147e73ff9755606e7
Alfian Rudi Yogatama <alvmarjinal@gmail.com>
2021-10-19 21:17:35 +0700

    nodemailer pull

--- client/client-dashboard-clinic/dashboard-clinic/src/App.js
+++ client/client-dashboard-clinic/dashboard-clinic/src/App.js
@@ -19,10 +19,10 @@ function App() {
       <Route path="/login">
         <Login />
       </Route>
-      
-      <PrivateRoute path='/'>
+
+      <Route path="/">
         <Home />
-      </PrivateRoute>
+      </Route>
 
     </Switch> 
   );
