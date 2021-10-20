dedf93af88441e7c7765e74a00b804ffdb4b94d5
Handson Panggabean <hansenpanggabean8@gmail.com>
2021-10-21 05:18:45 +0700

    test

--- server/app/routers/routerRegistrationUser.js
+++ server/app/routers/routerRegistrationUser.js
@@ -4,7 +4,7 @@ const { authenticationUser } = require('../middlewares/authentication')
 const { authorizationUser } = require('../middlewares/authorization')
 
 router.use(authenticationUser)
-router.post('/', authorizationUser, ControllerRegistrationUser.createRegistration)
+router.post('/', ControllerRegistrationUser.createRegistration)
 router.get('/:id', authorizationUser, ControllerRegistrationUser.findOneRegistration)
 router.put('/:id', authorizationUser, ControllerRegistrationUser.editRegistration)
 
