ee09ca6abc584e031704314fb7a84ff632bb800a
JoenathanSent <jonathansentosa80@gmail.com>
2021-10-21 06:26:20 +0700

    buat routing edit status

--- server/app/middlewares/authorization.js
+++ server/app/middlewares/authorization.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 const { User, Registration } = require("../models");
 
-=======
-const { Registration, User } = require("../models");
->>>>>>> c32bfeed6b5594a6147a3ab8fde7b80c3e23a841
 const authorizationUser = async (req, res, next) => {
   const id = +req.params.id;
   try {
@@ -11,8 +7,6 @@ const authorizationUser = async (req, res, next) => {
     if (foundRegistration) {
       if (req.user.id == foundRegistration.UserId) {
         next();
-<<<<<<< HEAD
-=======
       } else {
         throw { name: "You are not authorized" };
       }
@@ -31,7 +25,6 @@ const authorizationUserProfile = async (req, res, next) => {
     if (foundUser) {
       if (req.user.id == foundUser.id) {
         next();
->>>>>>> c32bfeed6b5594a6147a3ab8fde7b80c3e23a841
       } else {
         throw { name: "You are not authorized" };
       }
@@ -42,27 +35,6 @@ const authorizationUserProfile = async (req, res, next) => {
     next(err);
   }
 };
-<<<<<<< HEAD
-
-const authorizationUserProfile = async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const foundUser = await User.findByPk(id);
-    if (foundUser) {
-      if (req.user.id == foundUser.id) {
-        next();
-      } else {
-        throw { name: "You are not authorized" };
-      }
-    } else {
-      throw { name: "Data Not Found" };
-    }
-  } catch (err) {
-    next(err);
-  }
-};
-=======
->>>>>>> c32bfeed6b5594a6147a3ab8fde7b80c3e23a841
 
 const authorizationClinic = async (req, res, next) => {
   const id = +req.params.id;
